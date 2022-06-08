f8fcabe08c67ad9bdb8a1d072ebc7013698318fb
zaid2526 <mzr16894@.com>
2022-06-08 15:48:02 +0530

    task 4 (async/await) Convert the createPost , deletePost into async await

--- asyncAwait.js
+++ asyncAwait.js
@@ -108,60 +108,122 @@ function getp(){
 // console.log("persone 5: shows ticket.");
 
 
-//task 3 of async/await....
+// //task 3 of async/await....
 
-console.log("persone 1 : shows ticket.");
-console.log("persone 2: shows ticket.");
-const preMovie = async () => {
+// console.log("persone 1 : shows ticket.");
+// console.log("persone 2: shows ticket.");
+// const preMovie = async () => {
 
-    const promiseWifeBringingTicket = new Promise((resolve, reject) => {
-        setTimeout(() => {
-           // resolve('ticket')
-           reject()
-        }, 3000)
+//     const promiseWifeBringingTicket = new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//            // resolve('ticket')
+//            reject()
+//         }, 3000)
+//     });
+//     const getPopCorn = new Promise((resolve, reject) => resolve('popcorn'));
+//     const getCandy = new Promise((resolve, reject) => resolve('candy'));
+//     const getColdDrinks = new Promise((resolve, reject) => resolve('cold drinks'));
+//     //these are not depends on eachother.. so we does not wait for any of other..
+//     let ticket;
+
+//     try {
+//         ticket = await promiseWifeBringingTicket;
+//         let [popCorn, candy, coldDrink] = await Promise.all([getPopCorn, getCandy, getColdDrinks])
+//         console.log(`${popCorn} ${candy} ${coldDrink}`);
+
+//     } catch (e) {
+//         ticket = 'sad face...';
+//     }
+//     return ticket;
+// }
+// preMovie().then((t) => console.log(`"persone 3 : shows ${t}"`));
+
+// //when promise depend on anothe promise...
+// const testUser=async()=>{
+//     const loadUserForm= new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("user form loaded");
+//            resolve('user form loaded')
+//         }, 3000)
+//     });
+
+//     const entreUserName= new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("username entered.");
+//            resolve('username entered...')
+//         }, 3000)
+//     });
+
+//     const verifyUserDetails= ()=>{
+//            return 'user verified...';
+//     }
+
+//     await loadUserForm;
+//     await entreUserName;
+//     const verifyDetails= verifyUserDetails();
+
+//     return verifyDetails;
+// }
+// testUser().then((t)=>console.log(t))
+
+
+
+//task 4 (async/await)......
+
+let posts = [
+    { title: "post one", body: "this is body one" },
+    { title: "post two", body: "this is body two" }
+]
+
+const userPost = async () => {
+    const getPost = () => {
+        setTimeout(()=>{
+            let output='';
+            posts.forEach((post)=>{
+                output+=`<li> ${post.title} </li>`;
+                document.body.innerHTML=output
+            });
+        },1000);
+    };
+
+    const createPost = (post) => {
+        return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            //posts.push(post);
+            let error=false;
+            if(!error){
+                resolve(posts.push(post));
+            }else{
+                reject("post not created")
+            }
+        },1000)
     });
-    const getPopCorn = new Promise((resolve, reject) => resolve('popcorn'));
-    const getCandy = new Promise((resolve, reject) => resolve('candy'));
-    const getColdDrinks = new Promise((resolve, reject) => resolve('cold drinks'));
-    //these are not depends on eachother.. so we does not wait for any of other..
-    let ticket;
-
-    try {
-        ticket = await promiseWifeBringingTicket;
-        let [popCorn, candy, coldDrink] = await Promise.all([getPopCorn, getCandy, getColdDrinks])
-        console.log(`${popCorn} ${candy} ${coldDrink}`);
-
-    } catch (e) {
-        ticket = 'sad face...';
     }
-    return ticket;
-}
-preMovie().then((t) => console.log(`"persone 3 : shows ${t}"`));
 
-//when promise depend on anothe promise...
-const testUser=async()=>{
-    const loadUserForm= new Promise((resolve, reject) => {
+    const deletePost = new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("user form loaded");
-           resolve('user form loaded')
-        }, 3000)
+            //posts.push(post);
+            let error = false;
+            if (!error) {
+                resolve(posts.pop());
+                console.log("deleted",posts)
+            } else {
+                reject("post not created")
+            }
+        }, 4000);
     });
 
-    const entreUserName= new Promise((resolve, reject) => {
-        setTimeout(() => {
-            console.log("username entered.");
-           resolve('username entered...')
-        }, 3000)
-    });
 
-    const verifyUserDetails= ()=>{
-           return 'user verified...';
-    }
+    getPost();
+    await createPost({ title: "post three", body: "this is body one" });
+    await deletePost;
+    getPost();
+    await createPost({ title: "post four", body: "this is body four" });
+    getPost();
+   
 
-    await loadUserForm;
-    await entreUserName;
-    const verifyDetails= verifyUserDetails();
-
-    return verifyDetails;
+    
+    
 }
-testUser().then((t)=>console.log(t))
+
+userPost();
