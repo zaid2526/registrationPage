41e8690cd265f3e4f90e895817eaece8365915d7
zaid2526 <mzr16894@.com>
2022-06-14 16:31:51 +0530

    task 11 getting the userDetails from crud crud and show it onpur website

--- crudCrudPractice/objectToServer.js
+++ crudCrudPractice/objectToServer.js
@@ -24,3 +24,44 @@ function runEvent(e){
     document.getElementById('phone').value='';
 }
 
+window.addEventListener('DOMContentLoaded',()=>{
+
+    axios.get('https://crudcrud.com/api/d952f862439846c3b9eb05426af3b4c2/appointmentData')
+        .then(response => {
+            console.log(response.data)
+            for (i = 0; i < response.data.length; i++) {
+                console.log("responses",response.data.length)
+                showOnScreen(response.data[i]);
+
+
+            }
+        })
+        .catch(err=>{
+            document.body.innerHTML=document.body.innerHTML + '<h4> Something went Wrong </h4>'
+            console.log(err);
+        })
+});
+
+function showOnScreen(user){
+   
+    var userList=document.getElementById('users');
+   
+    let li=document.createElement('li');
+    li.className='list-group-item';
+    li.id=`${user.email}`
+
+    li.appendChild(document.createTextNode(`${user.name} ${user.email} ${user.phone}`));
+    let dltBtn=document.createElement('button');
+    dltBtn.className='btn btn-danger float-right btn-sm  dlt';
+    dltBtn.appendChild(document.createTextNode('X'));
+    li.appendChild(dltBtn);
+    let editBtn=document.createElement('button');
+    
+    editBtn.appendChild(document.createTextNode('edit'))
+    
+    li.appendChild(editBtn);
+
+    userList.appendChild(li);
+    
+
+}
